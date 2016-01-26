89a04dcb291e01f3cc593186182e1e049c6d1723
Airã Carvalho <airacarvalho@hotmail.com>
2016-01-26 19:32:37 -0200

    Rock_Paper_Scissors_v0.2
    Small changes in the code, trying to implement some new things, like lists to the possible results.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,86 +1,87 @@
-# Aira Carvalho       -       January, 2016
-# Rock, Paper and Scissors
+# Game Name:			Rock, Paper and Scissors
+# Created by:			Aira Carvalho da Silva
+# Date:						01/26/2016
+
+''' The rules are simple: You have to choose between three options: "Rock, Paper or Scissors".
+The computer will also choose one. Rocks beats Scissors, Paper beats Rock and Scissors beats Paper.'''
+
+# Imports all modules needed to this script.
 import random
 
+# Keeps track of score.
 playerScore = 0
 cpuScore = 0
 
-def displayIntro():
-    
-    print "Welcome to Rock, Paper, Scissors."
-    print "Do you think you can beat me? Let's see about that."
+# Global lists of Possibilities
+possible_moves = ["rock", "paper", "scissors", "quit"]
 
-def playerChoose():
-    
-    playerMove = " "
-    
-    while playerMove != "rock" and playerMove != "paper" and playerMove != "scissors" and playerMove != "exit":
-        playerMove = raw_input("What's your move, Player (rock, paper or scissors)? Or type 'exit' to stop playing. ")
-    return playerMove
+# Asks for the Player's name.
+playerName = raw_input("Hello, what's your name? ")
 
-def cpuChoose():
-    
-    cpuMove = " "
-    
-    cpuRand = random.randint(1,3)
-    
-    while cpuMove != "rock" and cpuMove != "paper" and cpuMove != "scissors":
-        if cpuRand == 1:
-            cpuMove = "rock"
-            return cpuMove
-        elif cpuRand == 2:
-            cpuMove = "paper"
-            return cpuMove
-        else:
-            cpuMove = "scissors"
-            return cpuMove
+# This function asks the player to Choose what he wants to play.
+def playerMove():
 
+	playerChoose = " "
+	
+	while playerChoose not in possible_moves:
+		playerChoose = raw_input("Do you want to play 'Rock', 'Paper' or 'Scissors' (Or type 'Quit' to exit)? ").lower()
+	return playerChoose
+
+# This function chooses what the CPU is going to play next.
+def cpuMove():
+
+	cpuChoose = possible_moves[(random.randint(0,2))]
+	return cpuChoose
+
+# This function checks who's the Winner.
 def checkWinner(player, cpu):
-    
-    if player == "rock" and cpu == "paper":
-        return "cpu"
-        
-    elif player == "rock" and cpu == "scissors":
-        return "player"
-        
-    elif player == "scissors" and cpu == "paper":
-        return "player"
-        
-    elif player == "scissors" and cpu == "rock":
-        return "cpu"
-        
-    elif player == "paper" and cpu == "rock":
-        return "player"
-        
-    elif player == "paper" and cpu == "scissors":
-        return "cpu"
-        
-    else:
-        return "draw"
+	player_wins = "player_wins"
+	cpu_wins = "cpu_wins"
+	draw = "draw"
+	
+	if player == "rock" and cpu == "scissors":
+		return player_wins
+		
+	elif player == "paper" and cpu == "rock":
+		return player_wins
+		
+	elif player == "scissors" and cpu == "paper":
+		return player_wins
+		
+	elif player == "rock" and cpu == "paper":
+		return cpu_wins
+		
+	elif player == "paper" and cpu == "scissors":
+		return cpu_wins
+		
+	elif player == "scissors" and cpu == "rock":
+		return cpu_wins
+		
+	else:
+		return draw
 
-def totalScore():
-    print "The total score is:\nPlayer Score: %s \nCPU Score: %s" % (playerScore, cpuScore)
-
-displayIntro()
-
-playAgain = "yes"
-while playAgain == "yes" or playAgain == "y":
-
-    playerRound = playerChoose()
-    if playerRound == "exit":
-        break
-    
-    cpuRound = cpuChoose()
-    
-    winner = checkWinner(playerRound, cpuRound)
-    
-    if winner == "player":
-        print "Congratulations, you've won this round! Your move was '%s' and mine was '%s'." % (playerRound, cpuRound)
-        playerScore += 1
-    elif winner == "cpu":
-        print "In you face! I've won this round! Your move was '%s' and mine was '%s'." % (playerRound, cpuRound)
-        cpuScore += 1
-    else:
-        print "Well, that's a draw. Your move was '%s' and mine was '%s'." % (playerRound, cpuRound)
-
-    totalScore()
+# Main Loop for the Script. Calls all the functions.
+playAgain = True
+while playAgain == True:
+	
+	player_move = playerMove()
+	if player_move == "quit":
+		break
+	
+	cpu_move = cpuMove()
+	
+	final_result = checkWinner(player_move, cpu_move)
+	
+	if final_result == "player_wins":
+		playerScore += 1
+		print "Congratulations '%s', you've WON this round." % (playerName)
+	
+	elif final_result == "cpu_wins":
+		cpuScore += 1
+		print "I'm sorry '%s', but you've LOST this round." % (playerName)
+		
+	else:
+		print "Well, that was a DRAW."
+	
+	print "You've played '%s' and the CPU played '%s'." % (player_move, cpu_move)
+	print "The total score is: \nPlayer Score: %s \nCPU Score: %s" % (playerScore, cpuScore)
