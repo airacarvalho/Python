17ab9105ac3c4749804b39d98502e64f69c4804d
Airã Carvalho <airacarvalho@hotmail.com>
2016-01-31 23:07:32 -0200

    Update rock_paper_scissors.py
    Implemented a new module and worked on the "design" of the text based game.

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,12 +1,14 @@
 # Game Name:			Rock, Paper and Scissors
 # Created by:			Aira Carvalho da Silva
-# Date:						01/26/2016
+# Date:				01/26/2016
+# Python Version:		Python 2.x
 
 ''' The rules are simple: You have to choose between three options: "Rock, Paper or Scissors".
 The computer will also choose one. Rocks beats Scissors, Paper beats Rock and Scissors beats Paper.'''
 
 # Imports all modules needed to this script.
 import random
+import sys
 
 # Keeps track of score.
 playerScore = 0
@@ -24,14 +26,14 @@ def playerMove():
 	playerChoose = " "
 	
 	while playerChoose not in possible_moves:
-		playerChoose = raw_input("Do you want to play 'Rock', 'Paper' or 'Scissors' (Or type 'Quit' to exit)? ").lower()
+		playerChoose = raw_input("\nDo you want to play 'Rock', 'Paper' or 'Scissors' (Or type 'Quit' to exit)? ").lower()
 	return playerChoose
 
 # This function chooses what the CPU is going to play next.
 def cpuMove():
 
 	cpuChoose = possible_moves[(random.randint(0,2))]
-	return cpuChoose
+	return cpuChoose	
 
 # This function checks who's the Winner.
 def checkWinner(player, cpu):
@@ -60,28 +62,44 @@ def checkWinner(player, cpu):
 	else:
 		return draw
 
+# Winning Function
+def playerWins():
+	
+	global playerScore
+
+	playerScore += 1
+	print "\nCongratulations '%s', you've WON this round." % (playerName)
+
+# Loosing Function
+def playerLooses():
+	
+	global cpuScore
+
+	cpuScore += 1
+	print "\nI'm sorry '%s', but you've LOST this round." % (playerName)
+
 # Main Loop for the Script. Calls all the functions.
 playAgain = True
 while playAgain == True:
 	
 	player_move = playerMove()
 	if player_move == "quit":
-		break
+		sys.exit()
 	
 	cpu_move = cpuMove()
 	
 	final_result = checkWinner(player_move, cpu_move)
 	
 	if final_result == "player_wins":
-		playerScore += 1
-		print "Congratulations '%s', you've WON this round." % (playerName)
+		playerWins()
 	
 	elif final_result == "cpu_wins":
-		cpuScore += 1
-		print "I'm sorry '%s', but you've LOST this round." % (playerName)
+		playerLooses()
 		
 	else:
 		print "Well, that was a DRAW."
 	
-	print "You've played '%s' and the CPU played '%s'." % (player_move, cpu_move)
+	print " "
+	print "You've played '%s' and the CPU played '%s'. \n" % (player_move, cpu_move)
 	print "The total score is: \nPlayer Score: %s \nCPU Score: %s" % (playerScore, cpuScore)
+	print " "
